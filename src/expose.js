818e0a0e281a8da1f1800afbfe090ac4cd7ee7bc
christos constandinou <constantology@gmail.com>
2012-08-19 22:21:43 +0100

    tidied up exposé

--- src/expose.js
+++ src/expose.js
@@ -8,10 +8,10 @@
 		return n8iv;
 	}
 
-	util.iter( PACKAGE ) || ( PACKAGE = util.global );
+	util.iter( PACKAGE ) || ( PACKAGE = util.ENV == 'commonjs' ? module : util.global );
 
 // expose n8iv
-	util.def( ( __lib__ = util.expose( __lib__, Name, util.ENV == 'commonjs' ? module : util.global ) ), 'x', x, 'r' );
+	util.def( ( __lib__ = util.expose( __lib__, Name, PACKAGE ) ), 'x', x, 'r' );
 	util.expose( util, __lib__ );  // store a reference to m8 on n8iv
 
 	util.x( Object, Array, Function, Number, String );
